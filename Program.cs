f3610016ffbdf877e43bb15ff21e2757cde13130
marciomarquesdesouza <marciomarques.contato@hotmail.com>
2020-10-13 22:34:39 -0300

    Teste commit

--- Program.cs
+++ Program.cs
@@ -102,7 +102,7 @@ namespace Revisao
       Console.WriteLine("1 - Inserir novo aluno");
       Console.WriteLine("2 - Listar alunos");
       Console.WriteLine("3 - Calcular média geral");
-      Console.WriteLine("X - Sair!!!!!!!!!!!!!!!!");
+      Console.WriteLine("X - Sair!");
       Console.WriteLine();
 
       String opcaoUsuario = Console.ReadLine();
